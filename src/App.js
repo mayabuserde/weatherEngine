a9b6fc9d72eb5826a7c56179164ab50f878fda59
Maya Buser De <maya.buser@gmail.com>
2021-07-06 16:34:36 -0400

    adding Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-
+import Weather from "./Weather";
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
-     <h1>Hello from Weather</h1>
-<footer>
-<footer><a href="https://github.com/mayabuserde/weatherEngine" target="_blank" rel="noreferrer">Open-source code</a>, by <a href="https://mayabuserde.github.io/" target="_blank" rel="noreferrer">Maya Buser De</a></footer>
-</footer>
+     <div class="container">
+       <Weather />
+      
+       <footer><a href="https://github.com/mayabuserde/weatherEngine" target="_blank" rel="noreferrer">Open-source code</a>, by <a href="https://mayabuserde.github.io/" target="_blank" rel="noreferrer">Maya Buser De</a></footer>
+
+     </div>
     </div>
   );
 }
