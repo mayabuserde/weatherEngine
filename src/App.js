7d6d721bfd30fe6b671ecbde5533440544cce350
Maya Buser De <maya.buser@gmail.com>
2021-07-06 16:18:50 -0400

    Cleaning up repo and added footer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Hello from Weather</h1>
+<footer>
+<footer><a href="https://github.com/mayabuserde/weatherEngine" target="_blank" rel="noreferrer">Open-source code</a>, by <a href="https://mayabuserde.github.io/" target="_blank" rel="noreferrer">Maya Buser De</a></footer>
+</footer>
     </div>
   );
 }
-
-export default App;
