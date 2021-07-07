4dfc585ec7c3d76c059d28a5dea57578f6c6e89d
Maya Buser De <maya.buser@gmail.com>
2021-07-07 17:26:51 -0400

    working on date formatting

--- src/FormattedDate.js
+++ src/FormattedDate.js
@@ -5,6 +5,14 @@ export default function FormattedDate(props) {
     let hours = props.date.getHours();
     let minutes = props.date.getMinutes();
 
+    if(minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+
+    if(hours < 10) {
+        hours = `0${hours}`;
+    }
+
     return (
         <div>{days[props.date.getDay()]}, {hours}:{minutes}</div>
     )
--- src/Weather.js
+++ src/Weather.js
@@ -8,12 +8,14 @@ export default function Weather(props) {
 
 
     
+let [city, setCity] = useState(props.defaultCity);
 
 let [weather, setWeather] = useState({ready: false});
     function displayWeather(response) {
         setWeather({
             ready: true,
-            date: new Date(response.data.dt * 1000),
+            //to get local time zone add timezone data + 4 hours (why... I don't know)
+            date: new Date((response.data.dt + response.data.timezone + 14400) * 1000),
             name: response.data.name,
             temperature: Math.round(response.data.main.temp),
             humidity: response.data.main.humidity,
@@ -28,13 +30,27 @@ let [weather, setWeather] = useState({ready: false});
 
     }
     
+    function handleSubmit(event) {
+        event.preventDefault();
+        search();
+    }
 
-    if(weather.ready) {
+    function updateCity(event) {
+        setCity(event.target.value);
+    }
+
+    function search() {
+        const ApiKey = "ffa66c6f94c53b49251ff2fd2a6dec80";
+        let ApiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${ApiKey}`;
+            axios.get(ApiUrl).then(displayWeather);
+    }
+
+    if (weather.ready) {
     return (
         <div className="Weather">
         <div className="container">
-        <form className="row">
-        <div className="col-9"><input type="search" className="form-control" /></div>
+        <form className="row" onSubmit={handleSubmit}>
+        <div className="col-9"><input type="search" className="form-control" onChange={updateCity} /></div>
         <div className="col-3"><input type="submit" value="Search" className="btn btn-primary w-100"/></div>
         </form>
         <WeatherInfo data={weather} />
@@ -43,12 +59,10 @@ let [weather, setWeather] = useState({ready: false});
         </div>
         
     ) } else {
-        const ApiKey = "ffa66c6f94c53b49251ff2fd2a6dec80";
-    let ApiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&units=metric&appid=${ApiKey}`;
-        axios.get(ApiUrl).then(displayWeather);
+       search();
 
         return (
-            <h1>Loading ...{props.home}</h1>
+            <h1>Loading ...</h1>
         )
     }
 
